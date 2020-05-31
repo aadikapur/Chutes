e6ce5298ac8b335359e784c362f2a8f1e0b98135
aadikapur <aadikapur1@gmail.com>
2020-05-30 20:40:40 -0400

    heroku configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 const {addUser,removeUser,getUser} = require('./users.js')
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
@@ -50,4 +51,5 @@ io.on('connection', (socket) => {
 })
 
 app.use(router)
+app.use(cors())
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
